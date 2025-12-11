6512dfd52f7c76ba5e4ff8d6c149dc6d1b1f48fe
Malik Test <malekba3bad@gmail.com>
2025-12-11 10:14:54 +0300

    update and add github

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
-// import Github from "./Github";
+import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.svg";
@@ -47,7 +47,7 @@ function About() {
         </h1>
         <Toolstack />
 
-        {/* <Github /> */}
+        <Github />
       </Container>
     </Container>
   );
